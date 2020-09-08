dcdc4516d6afb0b01d8c72239e6e40cf3138a516
Amy Liu <liuamy840@hotmail.com>
2020-09-08 12:38:01 +0800

    feat: add scan qr button

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import './Tab3.css';
+import { usePhotoGallery, Photo } from '../hooks/usePhotoGallery';
+
 
 const Tab3: React.FC = () => {
+  const { deletePhoto, photos, takePhoto } = usePhotoGallery();
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,13 +14,15 @@ const Tab3: React.FC = () => {
           <IonTitle>Tab 3</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent style={{position: 'relative'}}>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonButton shape="round" fill="outline" color="dark" 
+          style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}
+          onClick={() => takePhoto()}>Scan QR Code</IonButton>
       </IonContent>
     </IonPage>
   );
