58ec5a75853800c0cebe795e45132440973467a7
Amy Liu <liuamy840@hotmail.com>
2020-09-09 07:38:27 +0800

    feat: check server status

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import './Tab1.css';
+import { usePhotoGallery, Photo } from '../hooks/usePhotoGallery';
 
-const Tab1: React.FC = () => {
+const API_SERVER = 'https://be-app-hiring-bixinf-test.22ad.bi-x.openshiftapps.com';
+const API_USER = 'admin';
+const API_PASSWORD = 'secret';
+
+type Props = { serverStatus: string; };
+
+function Tab1() {
+  const [serverStatus, setServerStatus] = useState<String>('');
+  const { deletePhoto, photos, takePhoto } = usePhotoGallery();
+
+  const checkServerStatus = () => {
+    return fetch(`${API_SERVER}/api/v1.0/status`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setServerStatus(data.status.toUpperCase())
+      });
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,13 +29,20 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent style={{position: 'relative'}}>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton shape="round" fill="outline" color="dark" 
+          style={{position: 'absolute', left: '50%', top: '30%', transform: 'translate(-50%, -50%)'}}
+          onClick={checkServerStatus}>Check Server Status</IonButton>
+        <p style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}
+          >Server: {serverStatus}</p>
+        <IonButton shape="round" fill="outline" color="dark" 
+          style={{position: 'absolute', left: '50%', top: '70%', transform: 'translate(-50%, -50%)'}}
+          onClick={takePhoto}>Take photo & upload it</IonButton>      
       </IonContent>
     </IonPage>
   );
